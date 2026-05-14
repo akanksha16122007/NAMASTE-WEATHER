3b145917729d95237d1e8349735aa2be496db71a
akanksha16122007 <akankshakhurana01@gmail.com>
2026-05-15 00:29:33 +0530

    commit changes alert

--- script.js
+++ script.js
@@ -55,31 +55,37 @@ async function getWeather() {
     if (temp > 30) {
       document.getElementById("temp").innerHTML =
         `<h3>☀️ Weather Type: Sunny</h3>`;
+      alert("🥵 It's hot! Stay hydrated 💧");
       document.body.style.backgroundImage =
         "url('https://media.istockphoto.com/id/1007768414/photo/blue-sky-with-bright-sun-and-clouds.jpg?s=612x612&w=0&k=20&c=MGd2-v42lNF7Ie6TtsYoKnohdCfOPFSPQt5XOz4uOy4=')";
     } else if (temp >= 20 && temp <= 30) {
       document.getElementById("temp").innerHTML =
         `<h3>☁️ Weather Type: Cloudy</h3>`;
+      alert("📸 Perfect aesthetic sky for photos!");
       document.body.style.backgroundImage =
         "url('https://t4.ftcdn.net/jpg/05/13/26/73/360_F_513267391_QEmNGeOFLLqrILTnoq21dReUPp5UsoNr.jpg')";
     } else if ((temp < 20 && temp >= 15) || rainChance > 50) {
       document.getElementById("temp").innerHTML =
         `<h3>🌧️ Weather Type: Rainy</h3>`;
+      alert("🌧️ Perfect weather for chai + pakoras 🍵");
       document.body.style.backgroundImage =
         "url('https://t4.ftcdn.net/jpg/16/52/66/41/360_F_1652664174_KHr08RB9DI3HT4s6aJQBEygZVYAVkM8y.jpg')";
     } else if (temp < 15 && temp >= 10) {
       document.getElementById("temp").innerHTML =
         `<h3>⛈️ Weather Type: Stormy</h3>`;
+      alert("⚡ Thunderstorm incoming! Stay indoors!");
       document.body.style.backgroundImage =
         "url('https://c4.wallpaperflare.com/wallpaper/5/528/863/lightning-thunder-sky-lightning-strikes-wallpaper-preview.jpg')";
     } else if ((temp < 10 && temp >= 0) || wind > 10) {
       document.getElementById("temp").innerHTML =
         `<h3>🌬️ Weather Type: Windy</h3>`;
+      alert("💨 It's windy! Hold your hair and your phone!");
       document.body.style.backgroundImage =
         "url('https://c0.wallpaperflare.com/preview/556/693/130/trees-path-palm-trees-wind.jpg')";
     } else if (temp < 0) {
       document.getElementById("temp").innerHTML =
         `<h3>❄️ Weather Type: Snowy</h3>`;
+      alert("🧦 Layer up or regret it!");
       document.body.style.backgroundImage =
         "url('https://images.unsplash.com/photo-1418985991508-e47386d96a71?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c25vdyUyMHdhbGxwYXBlcnxlbnwwfHwwfHx8MA%3D%3D')";
     }
